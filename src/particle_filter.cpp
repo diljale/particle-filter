16f367fb9c915f551617dd1e88971b9893f4a827
diljale <bhavendedhia@gmail.com>
2017-09-14 23:10:42 -0700

    Update particle_filter.cpp

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -25,7 +25,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-	num_particles = 500;
+	num_particles = 100;
 	weights.resize(num_particles, 1.0);
 
 	std::default_random_engine gen;
